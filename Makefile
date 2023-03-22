95d7c3452da4ebd957fa21efd659304342c8de1a
Your Name <you@example.com>
2023-03-22 12:03:35 +0500

    fifth

--- Makefile
+++ Makefile
@@ -2,5 +2,7 @@ publish:
 	npm publish --dry-run
 brain-games:
 	node bin/brain-games.js
+brain-even:
+	node bin/brain-even.js
 lint:
 	npx eslint .
---
+++ src/first.js
@@ -0,0 +1,34 @@
+import readlineSync from "readline-sync";
+
+console.log("Welcome to the Brain Games!");
+const username = readlineSync.question("May I have your name? ");
+console.log(`Hello, ${username}!`);
+console.log("Answer \"yes\" if the number is even, otherwise answer \"no.\"");
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+let score = 0;
+let CorrectAnswer = "";
+const isEven = (mainnumber) => mainnumber % 2 === 0;
+// eslint-disable-next-line no-mixed-operators
+const CorrectYes = (mainnumber, firstanswer) => isEven(mainnumber) && firstanswer === "yes" || !isEven(mainnumber) && firstanswer === "no";
+// eslint-disable-next-line no-constant-condition
+while (true) {
+  const mainnumber = getRandomInt(50);
+  console.log(`Question: ${mainnumber}`);
+  const firstanswer = readlineSync.question("Your answer: ");
+  if (isEven(mainnumber) === true) {
+    CorrectAnswer = "yes";
+  } else if (isEven(mainnumber) === false) {
+    CorrectAnswer = "no";
+  }
+  if (!CorrectYes(mainnumber, firstanswer)) {
+    console.log(`"${firstanswer}" is wrong answer. Correct answer was "${CorrectAnswer}" Let's try again`);
+    break;
+  }
+  console.log("Correct!");
+  score += 1;
+  if (score === 3) {
+    console.log(`Congratulations, ${username}`);
+    break;
+  }
+}
+export default CorrectYes;
