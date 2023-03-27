5e773e76dd53ec60b46f0b935749c9b23be034ee
Your Name <you@example.com>
2023-03-27 12:35:10 +0500

    last step

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from "readline-sync";
 
 console.log("Welcome to the Brain Games!");
-export const username = readlineSync.question("May I have your name?");
+const username = readlineSync.question("May I have your name?");
 console.log(`Hello, ${username}!`);
+export default username;
