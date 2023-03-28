755711f7e967e1dec599a5c98a486a43d3ca29b8
kazualHD <118461693+kazualHD@users.noreply.github.com>
2023-03-28 20:43:52 +0500

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!");
-const username = readlineSync.question("May I have your name?");
+console.log('Welcome to the Brain Games!');
+const username = readlineSync.question('May I have your name?');
 console.log(`Hello, ${username}!`);
 export default username;
