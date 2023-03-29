34630efc1c276809fd721dcb50c323e8d2d40936
Your Name <you@example.com>
2023-03-29 22:05:41 +0500

    push pls

--- games/thirdgame.js
+++ games/thirdgame.js
@@ -1,24 +1,19 @@
+/* eslint-disable import/no-duplicates */
 import readlineSync from 'readline-sync';
 import username from '../src/index.js';
+import { check } from '../src/index.js';
 
 let score = 0;
-let correctAnswer = '';
-while (score < 3) {
+const correctAnswer = '';
+while (score < 3) { 
   let num1 = Math.floor(Math.random() * 100);
   let num2 = Math.floor(Math.random() * 100);
   console.log('Find the greatest common divisor of given numbers');
   console.log(`Question: ${num1} ${num2}`);
-  while (num1 !== 0 && num2 !== 0) {
-    if (num1 > num2) {
-      num1 %= num2;
-    } else {
-      num2 %= num1;
-    }
-  }
-  correctAnswer = num1 + num2;
+  const result = check(num1, num2);
   const userAnswer = parseInt(readlineSync.question('Your answer: '), 10);
-  if (correctAnswer !== userAnswer) {
-    console.log(`'${userAnswer}' is wrong answer. Correct answer was '${correctAnswer}'. Let's try again, ${username}!`);
+  if (check(num1, num2) !== userAnswer) {
+    console.log(`'${userAnswer}' is wrong answer. Correct answer was '${result}'. Let's try again, ${username}!`);
     break;
   }
   score += 1;
--- src/index.js
+++ src/index.js
@@ -7,9 +7,19 @@ let CorrectAnswer = '';
 export const CheckCorrectAnwer = (func, mainnumber) => {
   if (func(mainnumber) === true) {
     CorrectAnswer = 'yes';
-} else if (func(mainnumber) !== true ) {
+  } else if (func(mainnumber) !== true) {
     CorrectAnswer = 'no';
-}
+  }
   return CorrectAnswer;
-}
+};
+export const check = (num1, num2) => {
+  while (num1 !== 0 && num2 !== 0) {
+    if (num1 > num2) {
+      num1 %= num2;
+    } else {
+      num2 %= num1;
+    }
+  }
+  return num1 + num2;
+};
 export default username;
