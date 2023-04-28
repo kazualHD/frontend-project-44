421d77b74030db1a0025efb5734be194f6b2dac3
kazualHD <118461693+kazualHD@users.noreply.github.com>
2023-04-28 14:25:50 +0500

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -3,4 +3,3 @@ import { generateEvenRound } from '../utils.js';
 
 const taskDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 export default () => wrapper(taskDescription, generateEvenRound);
-
