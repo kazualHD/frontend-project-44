e4402486619abac7714b49a2387986c2f21fc6f5
kazualHD <118461693+kazualHD@users.noreply.github.com>
2023-05-02 19:09:57 +0500

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,5 +1,14 @@
 import wrapper from '../index.js';
-import { generateEvenRound } from '../utils.js';
+import getRandomInRange from '../utils.js';
 
 const taskDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
-export default () => wrapper(taskDescription, generateEvenRound);
+const isEven = (num) => num % 2 === 0;
+
+const generateRound = () => {
+  const num = getRandomInRange(1, 100);
+  const question = String(num);
+  const answer = isEven(num) ? 'yes' : 'no';
+  return [question, answer];
+};
+
+export default () => wrapper(taskDescription, generateRound);
