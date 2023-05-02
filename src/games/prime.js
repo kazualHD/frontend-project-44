9a9c8c4f7c687d99b348d61e77a7b9e53b45bf9e
kazualHD <118461693+kazualHD@users.noreply.github.com>
2023-05-02 19:10:28 +0500

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,6 +1,26 @@
 import wrapper from '../index.js';
-import { generatePrimeRound } from '../utils.js';
+import getRandomInRange from '../utils.js';
 
 const taskDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-export default () => wrapper(taskDescription, generatePrimeRound);
+const isPrime = (num) => {
+  let result;
+  if (num === 2) {
+    return true;
+  }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+    result = true;
+  }
+  return result;
+};
+
+const generateRound = () => {
+  const num = getRandomInRange(2, 100);
+  const question = String(num);
+  const answer = isPrime(num) ? 'yes' : 'no';
+  return [question, answer];
+};
+export default () => wrapper(taskDescription, generateRound);
