f368a2d0b0244841669d9edc297cbf04938c5b90
kazualHD <118461693+kazualHD@users.noreply.github.com>
2023-05-02 18:34:05 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import readlineSync from 'readline-sync';
 
 const ROUNDS_COUNT = 3;
-const wrapper = (taskDecription, generateRound) => {
+const wrapper = (taskDescription, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  console.log(taskDecription);
+  console.log(taskDescription);
   for (let i = 0; i < ROUNDS_COUNT; i += 1) {
     const [question, answer] = generateRound();
     console.log('Question:', question);
