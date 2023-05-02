3f72583cb261bb570ae311b26431290081d70731
kazualHD <118461693+kazualHD@users.noreply.github.com>
2023-05-02 19:09:33 +0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -3,29 +3,3 @@ const getRandomInRange = (min, max) => {
   return rand;
 };
 export default getRandomInRange;
-
-const isPrime = (num) => {
-  if (num <= 1) {
-    return false;
-  }
-  for (let i = 2; i <= Math.sqrt(num); i += 1) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return true;
-};
-
-export const generatePrimeRound = () => {
-  const num = getRandomInRange(2, 100);
-  const question = String(num);
-  const answer = isPrime(num) ? 'yes' : 'no';
-  return [question, answer];
-};
-const isEven = (num) => num % 2 === 0;
-export const generateEvenRound = () => {
-  const num = getRandomInRange(1, 100);
-  const question = String(num);
-  const answer = isEven(num) ? 'yes' : 'no';
-  return [question, answer];
-};
